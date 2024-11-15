8d19db53fdd5d4d7efb80527d1872c3b31771e01
24LP8255 24LP8255 <24LP8255@G301-CC0203-2.local>
2024-11-15 18:35:51 +0800

    oorchlolt

---
+++ new.js
@@ -0,0 +1,46 @@
+function funct1(){
+    document.getElementById("b1").style.display="block";
+    document.getElementById("b3").style.display="none";
+    document.getElementById("b2").style.display="none";
+    document.getElementById("dot1").style.backgroundColor="lightblue";
+    document.getElementById("dot2").style.backgroundColor="aqua";
+    document.getElementById("dot3").style.backgroundColor="aqua"
+ }
+function funct2(){
+    document.getElementById("b2").style.display="block";
+    document.getElementById("b1").style.display="none";
+    document.getElementById("b3").style.display="none";
+    document.getElementById("dot2").style.backgroundColor="lightblue";
+    document.getElementById("dot3").style.backgroundColor="aqua";
+    document.getElementById("dot1").style.backgroundColor="aqua"
+}
+function funct3(){
+    document.getElementById("b3").style.display="block"
+    document.getElementById("b1").style.display="none"
+    document.getElementById("b2").style.display="none";
+    document.getElementById("dot3").style.backgroundColor="lightblue";
+    document.getElementById("dot2").style.backgroundColor="aqua";
+    document.getElementById("dot1").style.backgroundColor="aqua"
+}
+function foward(){
+    document.getElementById("sr1").style.display="none";
+    document.getElementById("sr7").style.display="block";
+    document.getElementById("story-button1").style.display="block"}
+function back(){
+    document.getElementById("sr1").style.display="block";
+    document.getElementById("sr7").style.display="none"
+}
+function day1(){
+    document.getElementById("day2").style.color="grey";
+    document.getElementById("day2button").style.backgroundColor="grey";
+    document.getElementById("day1button").style.backgroundColor="blue";
+    document.getElementById("day1").style.color="blue";
+    document.getElementById("today").style.color="blue"
+}
+
+function day2(){
+    document.getElementById("day1").style.color="grey";
+    document.getElementById("day1button").style.backgroundColor="grey";
+    document.getElementById("day2").style.color="blue";
+    document.getElementById("day2button").style.backgroundColor="blue"
+}
